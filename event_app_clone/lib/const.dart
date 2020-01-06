05fc985f6051cf2b377b69bfa26cd1963acabb6a
Jignesh <Jigensh.shakya1997pro@gmail.com>
2020-01-06 23:48:24 +0530

    changes

--- event_app_clone/lib/const.dart
+++ event_app_clone/lib/const.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class ColorsConst{
 static Color greyColor="#a7a8a8".toHexColor();
 static Color blueText="#655BFF".toHexColor();
+static Color blueLight="#F4F3FF".toHexColor();
 }
--- event_app_clone/lib/main.dart
+++ event_app_clone/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import "package:flutter_dtx/flutter_dtx.dart";
 import 'const.dart';
 
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -53,21 +52,13 @@ class _MyHomePageState extends State<MyHomePage> {
         unselectedItemColor: Colors.black,
         items: [
           BottomNavigationBarItem(
-           title: SizedBox.shrink(),
-           icon:  Icon(Icons.home)  
-          ),
+              title: SizedBox.shrink(), icon: Icon(Icons.home)),
           BottomNavigationBarItem(
-           title:SizedBox.shrink(),
-           icon:  Icon(Icons.calendar_today)
-          ),
-         BottomNavigationBarItem(
-           title:SizedBox.shrink(),
-           icon:  Icon(Icons.search)
-          ),
+              title: SizedBox.shrink(), icon: Icon(Icons.calendar_today)),
           BottomNavigationBarItem(
-           title:SizedBox.shrink(),
-           icon:  Icon(Icons.person)
-          ),
+              title: SizedBox.shrink(), icon: Icon(Icons.search)),
+          BottomNavigationBarItem(
+              title: SizedBox.shrink(), icon: Icon(Icons.person)),
         ],
       ),
       body: Column(
@@ -111,105 +102,159 @@ class _MyHomePageState extends State<MyHomePage> {
                               ))
                         ],
                       ),
-
                     ],
                   ),
                 ),
                 Container(
                   alignment: Alignment.centerLeft,
-                  margin: EdgeInsets.only(top: 20,left: 20),
-                  child: Text("Suggested Themes",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+                  margin: EdgeInsets.only(top: 20, left: 20),
+                  child: Text(
+                    "Suggested Themes",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
                 ),
-                
                 Container(
-                  margin: const EdgeInsets.only(top: 10,right: 20,left: 20),
+                  margin: const EdgeInsets.only(top: 10, right: 20, left: 20),
                   child: Row(
                     children: <Widget>[
                       Expanded(
-                      child: Text("12 Suggested Themes",style: TextStyle(color: ColorsConst.greyColor,fontSize: 12,fontWeight: FontWeight.w800),
+                          child: Text(
+                        "12 Suggested Themes",
+                        style: TextStyle(
+                            color: ColorsConst.greyColor,
+                            fontSize: 12,
+                            fontWeight: FontWeight.w800),
                       )),
-                      Text("See all",style: TextStyle(color: ColorsConst.blueText),)
+                      Text(
+                        "See all",
+                        style: TextStyle(color: ColorsConst.blueText),
+                      )
                     ],
                   ),
                 ),
                 Container(
-                  height: context.getDeviceSize().width/2.8,
-                  width: double.maxFinite,
-                  margin: EdgeInsets.only(top: 20,left: 20),
-                  child: ListView.separated(
-                    itemCount: 5,
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (con,_){
-                      return Container(
-                        height: con.getDeviceSize().width/2.0,
-                        width: con.getDeviceSize().width/3.8,
+                    height: context.getDeviceSize().width / 2.8,
+                    width: double.maxFinite,
+                    margin: EdgeInsets.only(top: 20, left: 20),
+                    child: ListView.separated(
+                      itemCount: 5,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (con, _) {
+                        return Container(
+                          height: con.getDeviceSize().width / 2.0,
+                          width: con.getDeviceSize().width / 3.8,
                           child: Column(
-                          children: <Widget>[
-                            Container(
-                              height: con.getDeviceSize().width/3.6,
-                              width: con.getDeviceSize().width/2.8,
-                              decoration: BoxDecoration(
-                                color: Colors.indigoAccent,
-                                borderRadius: BorderRadius.circular(10)
+                            children: <Widget>[
+                              Container(
+                                height: con.getDeviceSize().width / 3.6,
+                                width: con.getDeviceSize().width / 2.8,
+                                decoration: BoxDecoration(
+                                    color: Colors.indigoAccent,
+                                    borderRadius: BorderRadius.circular(10)),
                               ),
-                              
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 8),
-                              child: Text("Food",style: TextStyle(color: Colors.black,fontWeight: FontWeight.w700)))
-                          
-                          ],
-                        ),
-                        
-                      );
-                    },
-                    separatorBuilder: (context,postion){
-                      return SizedBox(
-                      width: 6,
-                      );
-
-                    },
-
-                  )
-                )
-
+                              Padding(
+                                  padding: const EdgeInsets.only(top: 8),
+                                  child: Text("Food",
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w700)))
+                            ],
+                          ),
+                        );
+                      },
+                      separatorBuilder: (context, postion) {
+                        return SizedBox(
+                          width: 6,
+                        );
+                      },
+                    ))
               ],
             ),
           ),
           Expanded(
             child: Container(
               margin: EdgeInsets.only(left: 20),
-                        
-              
               child: ListView.separated(
                 itemCount: 2,
                 scrollDirection: Axis.vertical,
-                itemBuilder: (context,_){
+                itemBuilder: (context, _) {
                   return Container(
-                    height: 250,
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 20,top: 10,bottom: 10),
-                      child: Column(
-                        children: <Widget>[
-                          Text("Best of Vienna Waking Tour",style: TextStyle(),)
-                        ],
+                      height: 250,
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        margin: EdgeInsets.only(left: 50, top: 20, bottom: 10),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              child: Text(
+                                "Best of Vienna Waking Tour",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 18),
+                              ),
+                              alignment: Alignment.centerLeft,
+                            ),
+                            Container(
+                              alignment: Alignment.centerLeft,
+                              margin: EdgeInsets.only(top: 10),
+                              child: Row(
+                                children: <Widget>[
+                                  Padding(
+                                    padding:EdgeInsets.only(right: 10),
+                                    child: Chip(
+                                        backgroundColor: ColorsConst.blueLight,
+                                        label: Text(
+                                          "Travelling",
+                                          style: TextStyle(
+                                              color: ColorsConst.blueText),
+                                        )),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(right: 10),
+                                    child: Chip(
+                                        backgroundColor: ColorsConst.blueLight,
+
+                                        label: Text(
+                                          "History",
+                                          style: TextStyle(
+                                              color: ColorsConst.blueText),
+                                        )),
+                                  ),
+                                  Container(
+                                    height: 5,
+                                    width: 5,
+                                    alignment: Alignment.centerLeft,
+                                    margin: EdgeInsets.only(right: 8),
+                                    decoration: ShapeDecoration(
+                                      shape: CircleBorder(),
+                                      color: ColorsConst.greyColor
+                                    ),
+                                  ),
+                                  Text("starts at 10:30",style: TextStyle(color: ColorsConst.greyColor),)
+                                ],
+                                mainAxisSize: MainAxisSize.min,
+                              ),
+                            ),
+                            Expanded(
+                              child: Stack(
+                                children: <Widget>[
+
+
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  
-                width: double.maxFinite,
-                decoration: BoxDecoration(
-                color: Colors.white,
-            
-              
-                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(50),topLeft: Radius.circular(50))
-    
-                  ));
-                
+                      width: double.maxFinite,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(50),
+                              topLeft: Radius.circular(50))));
                 },
-                separatorBuilder: (__,___){
+                separatorBuilder: (__, ___) {
                   return SizedBox(height: 10);
                 },
-
               ),
             ),
           )
